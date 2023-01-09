9267c6937a63d280b4553f510c9e7901fb5e65e8
amin-leon <leoneandela@gmail.com>
2023-01-09 21:26:47 +0200

    scratch

---
+++ login/login.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCH-rRMi42mEFOY7AbZgxsRkqrXBI3wkY8",
+    authDomain: "fire9db-ebc8e.firebaseapp.com",
+    projectId: "fire9db-ebc8e",
+    storageBucket: "fire9db-ebc8e.appspot.com",
+    messagingSenderId: "96832741949",
+    appId: "1:96832741949:web:5a1c7c3556b3968a898054"
+  };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // let us code
+
+    var datab = 
